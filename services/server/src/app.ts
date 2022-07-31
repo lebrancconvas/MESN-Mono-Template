08fd5097669debd866b999702adfc22e3fa12d00
lebrancconvas <lebrancconvas@gmail.com>
2022-08-01 06:40:11 +0700

    🔊 feat: add logging

--- services/server/src/app.ts
+++ services/server/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import router from './routes/router';
 import { config } from './config/config';
+// import Logging from './log/logging';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
@@ -18,7 +19,7 @@ mongoose
 	})
 	.catch(err => {
 		console.error(err);
-		console.log('[FAILURE] Can\'t connected to MongoDB.')
+		console.error('[FAILURE] Can\'t connected to MongoDB.');
 	})
 
 app.listen(PORT, () => {
